b6bb0102ddbd479cb3392a91dc48765e826e0077
Lori A. Burns <lori.burns@gmail.com>
2023-07-18 02:06:25 -0400

    force netlib

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-    ./build_libtensor.py -v -d build --install ${PREFIX} --type Release --features libxm
+    ./build_libtensor.py -v -d build --install ${PREFIX} --type Release --features netlib libxm
 else
     ./build_libtensor.py -v -d build --install ${PREFIX} --type Release --features mkl libxm
 fi
