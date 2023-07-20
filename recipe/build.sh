050a47f379166730ee195741a311081fe4bac064
Lori A. Burns <lori.burns@gmail.com>
2023-07-20 03:46:04 -0400

    force clang 11 for mac (#4)

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,3 +3,14 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
 else
     ./build_libtensor.py -v -d build --install ${PREFIX} --type Release --features mkl libxm
 fi
+
+nm $PREFIX/lib/libtensorlight$SHLIB_EXT | grep product_table_container | grep get_instance
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    cd build
+    if [ "$(uname)" == "Linux" ]; then
+        ctest -E ewmult2
+    else
+        ctest
+    fi
+fi
