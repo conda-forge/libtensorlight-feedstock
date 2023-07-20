d1e19e2732940237d765fa6485ff8d7cf4a8f1a4
Lori A. Burns <lori.burns@gmail.com>
2023-07-20 00:39:48 -0400

    force clang 11 for mac

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,3 +3,5 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
 else
     ./build_libtensor.py -v -d build --install ${PREFIX} --type Release --features mkl libxm
 fi
+
+nm $PREFIX/lib/libtensorlight$SHLIB_EXT | grep product_table_container | grep get_instance
